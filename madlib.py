ff778db72b0743438424b610e963cfc2f819a565
Mike Mambwe <88389755+aeprodigy@users.noreply.github.com>
2022-10-05 01:20:42 +0200

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,8 @@
+#madlib app
+adj = input("Adjectivr:")
+verb1 = input("Verb1:")
+verb2 = input("Verb2:")
+madlib = f"Computer programming is so {adj}! it makes me go nuts\n I love this, I as born for this, I am {verb1} and i am too {verb2}"
+
+print(madlib)
+#madlib game for python beginnerss
